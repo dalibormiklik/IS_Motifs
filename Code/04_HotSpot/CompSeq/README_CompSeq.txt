b0c99b4503f6d49ba1cc4d652e11994c268dd863
dalibormiklik <112612710+dalibormiklik@users.noreply.github.com>
2022-09-27 12:11:12 +0200

    Delete Code directory

--- Code/04_HotSpot/CompSeq/README_CompSeq.txt
+++
@@ -1,2 +0,0 @@
-SeqMemFun is the main script with all the information in it.
-Functions.R and Load_mixPPM.R are suportive scripts that load data or functions. 
--- Code/04_HotSpot/rmsk_to_BED.sh
+++
@@ -1,9 +0,0 @@
-#Scripts for selecting particular repeats from repeatmasker and transform outut to BED format
-#Scripts are performed in the directory where rmsk.txt file downloaded from UCSC Goldenpath is saved
-
-#all repeats to BED
-awk 'BEGIN{OFS="\t"} {print $6,$7,$8,$11,$2,$10}' rmsk.txt > rmsk.bed
-
-#Alu
-awk 'BEGIN{OFS="\t"} $13 == "Alu" {print $6,$7,$8,$11,$2,$10}' rmsk.txt > rmsk_Alu.bed
-
